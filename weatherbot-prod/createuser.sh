207f3c40bff65fcfd3c48ffefab1161403c7701f
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-03 10:30:15 +0200

    Add files via upload

---
+++ weatherbot-prod/createuser.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#sudo -u postgres psql postgres;
+
+#create database
+sudo -u postgres bash -c "psql -c\"CREATE DATABASE weatherbot-prod;\""
+
+#create user 
+sudo -u postgres bash -c "psql -c \"CREATE USER weather_admin WITH PASSWORD 'weather?';\""
+sudo -u postgres bash -c "psql -c \"ALTER ROLE weather_admin set client_encoding to 'utf8';\""
+#sudo -u postgres bash -c "psql -c \"ALTER ROLE weather_admin set default_transaction_isolation to 'read committed';\""
+
+sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE weatherbot-prod to weather_admin;\""
+
