834cd97f72480b150774f1206bffd2b7dc21539f
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-03 12:55:35 +0200

    Update logs.tf

--- modules/ecs-cluster/logs.tf
+++ modules/ecs-cluster/logs.tf
@@ -1,17 +1,17 @@
 # logs.tf
 
 # Set up CloudWatch group and log stream and retain logs for 30 days
-resource "aws_cloudwatch_log_group" "cb_log_group" {
+resource "aws_cloudwatch_log_group" "cb_log_group_prod" {
   name              = "/ecs/cb-weatherbot-prod"
   retention_in_days = 30
 
   tags = {
-    Name = "cb-log-group"
+    Name = "cb-log-group-prod"
   }
 }
 
-resource "aws_cloudwatch_log_stream" "cb_log_stream" {
+resource "aws_cloudwatch_log_stream" "cb_log_stream_prod" {
   name           = "${var.app_name}-${var.environment}-log-stream"
-  log_group_name = aws_cloudwatch_log_group.cb_log_group.name
+  log_group_name = aws_cloudwatch_log_group.cb_log_group_prod.name
 }
 
