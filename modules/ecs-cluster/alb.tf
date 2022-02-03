901b2166504a98eafb90366a85335a61285e08af
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-03 23:14:14 +0200

    Update alb.tf

--- modules/ecs-cluster/alb.tf
+++ modules/ecs-cluster/alb.tf
@@ -6,22 +6,22 @@ resource "aws_alb" "main" {
   security_groups = [aws_security_group.lb-sg.id]
 }
 
-resource "aws_alb_target_group" "weatherbot" {
+resource "aws_alb_target_group" "weatherbot-dev" {
   name        = "${var.app_name}-${var.environment}-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = var.health_check_path
-    unhealthy_threshold = "2"
-  }
+  #health_check {
+    #healthy_threshold   = "3"
+    #interval            = "30"
+    #protocol            = "HTTP"
+    #matcher             = "200"
+    #timeout             = "3"
+    #path                = var.health_check_path
+    #unhealthy_threshold = "2"
+  #}
 }
 
 # Traffic from the ALB to the target group
@@ -31,7 +31,7 @@ resource "aws_alb_listener" "listener" {
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_alb_target_group.weatherbot.id
+    target_group_arn = aws_alb_target_group.weatherbot-dev.id
     type             = "forward"
   }
 }
