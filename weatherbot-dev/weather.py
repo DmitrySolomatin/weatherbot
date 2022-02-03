7567b9d6021e51a3507d6435975fc6052a6c27de
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-03 14:55:35 +0200

    Add files via upload

---
+++ weatherbot-dev/weather.py
@@ -0,0 +1,83 @@
+import os
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import CallbackContext, MessageHandler, Filters
+from weather_json import get_weather, geo_weather, get_weather_5
+import os
+import telegram
+import logging
+#from telegram import Update
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#logging
+logging.basicConfig(
+    format ='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
+logger = logging.getLogger(__name__)
+
+def start_handler(update:Updater, context:CallbackContext):
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text='Hello!')
+    update.message.reply_text("Type /help for instructions.")
+
+
+def help_handler(update:Updater, context: CallbackContext):
+    update.message.reply_text(
+        "Write /weather city.\nCity is where you want to know daily weather forecast."
+        "\nFor example, \n\t\t\t/weather Berlin\n\t\t\t/weather Kiev. \n"
+        "/weather5 city. It's a 5 day weather forecast.\n"
+        "/geo, to know daily weather forecast by location.\n"
+    )
+
+def geo_text_handler(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Send your location.")
+
+def geo_handler(update, context):
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+    result = geo_weather(lon, lat)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+
+
+def geo_text_handler_5(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Send your location.")
+
+def geo_handler_5(update, context):
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+    result_5 = geo_weather_5(lon, lat)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result_5)
+
+def temperature_handler(update, context):
+    if context.args:
+        loc = "".join(context.args)
+        result = get_weather(loc)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="".join(result))
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="/weather city\nWrite city name as a argument.")
+
+def temperature_handler_5(update, context):
+    if context.args:
+        loc = "".join(context.args)
+        result_5 = get_weather_5(loc)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="".join(result_5))
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="/weather5 city\nWrite city name as a argument.")
+
+def main():
+    APP_TOKEN = os.getenv('APP_TOKEN')
+    updater = Updater(APP_TOKEN ,use_context=True)
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler("help", help_handler))
+    updater.dispatcher.add_handler(CommandHandler("weather", temperature_handler))
+    updater.dispatcher.add_handler(CommandHandler("weather5", temperature_handler_5))
+    updater.dispatcher.add_handler(CommandHandler("geo", geo_text_handler))
+    updater.dispatcher.add_handler(MessageHandler(Filters.location, geo_handler))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
---
+++ weatherbot-dev/weather_json.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import os
+import math
+from datetime import datetime, timedelta
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('APP_WEATHER_KEY')
+
+def get_weather(loc):
+    try:
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'.format(loc, API_KEY)
+        data = requests.get(url)
+        response = json.loads(data.content.decode('utf8'))
+        city = response['name']
+        country = response['sys']['country']
+        temp = math.ceil(response['main']['temp'])
+        feels_like = math.ceil(response['main']['feels_like'])
+        main = [i['description'] for i in response['weather']]
+        day = datetime.today().strftime('%a (%m-%d)')
+        msg = '{}. Daily weather forecast in {}, {}: {}°C, feels like {}°C, {}.'.format(day, city, country, temp,
+                                                                                        feels_like, "".join(main))
+    except:
+        pass
+
+def get_weather_5(loc):
+    url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&cnt=5&appid={}'.format(loc, API_KEY)
+    data = requests.get(url)
+    response = json.loads(data.content.decode('utf8'))
+    # json request has {keys:values} pairs, data separation
+    cityn = response['city']['name']
+    country = response['city']['country']
+    datalist = response['list']
+    count = 0  # day count
+    day = []  # for day list
+    msg = []  # for message list
+    # data as a nested dictionary, getting their
+    for i in range(0, len(datalist)):
+        temp = [math.ceil(i['main']['temp']) for i in datalist]
+        feels_like = [math.ceil(i['main']['feels_like']) for i in datalist]
+        main = [i['weather'] for i in response['list']]
+        spec = [item['description'] for sublist in main for item in sublist]
+        count += i
+        day.append((datetime.today() + timedelta(days=count)).strftime('%a (%m-%d)'))
+        msg.append(day[i] + ": " + str(temp[i]) + "°C, feels like " + str(feels_like[i]) + "°C, " + str(spec[i]))
+    msgg = '{},{}. 5 Day Weather Forecast: '.format(cityn, country)
+    return msgg + "\n" + "\n".join([msg[i] for i in range(0, len(msg))])
+
+def geo_weather(lon, lat):
+    url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid={}'.format(lat, lon, API_KEY)
+    data = requests.get(url)
+    response = json.loads(data.content.decode('utf8'))
+    city = response['name']
+    country = response['sys']['country']
+    temp = math.ceil(response['main']['temp'])
+    feels_like = math.ceil(response['main']['feels_like'])
+    main = [i['description'] for i in response['weather']]
+    day = datetime.today().strftime('%a (%m-%d)')
+    msg = '{}. Daily weather forecast in {}, {}: {}°C, feels like {}°C, {}.'.format(day, city, country, temp,
+                                                                                    feels_like, "".join(main))
+    return msg
