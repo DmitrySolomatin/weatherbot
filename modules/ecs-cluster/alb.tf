6dc79379fb795a3cc684caf94c356d4f041a1dcb
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-03 21:08:02 +0200

    Update alb.tf

--- modules/ecs-cluster/alb.tf
+++ modules/ecs-cluster/alb.tf
@@ -13,16 +13,16 @@ resource "aws_alb_target_group" "weatherbot-prod" {
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = var.health_check_path
-    unhealthy_threshold = "2"
-  }
-}
+  #health_check {
+    #healthy_threshold   = "3"
+    #interval            = "30"
+    #protocol            = "HTTP"
+    #matcher             = "200"
+    #timeout             = "3"
+    #path                = var.health_check_path
+    #unhealthy_threshold = "2"
+  #}
+#}
 
 # Traffic from the ALB to the target group
 resource "aws_alb_listener" "listener" {
