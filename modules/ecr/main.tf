0a6171be330e471d35a41d582fe90ba115a32653
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-09 22:56:50 +0200

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,3 +1,9 @@
+# Repository for telegram bot
 resource "aws_ecr_repository" "ecr_repository" {
   name = local.repository_name
+}
+
+# Repository for web-page
+resource "aws_ecr_repository" "ecr_repository_page" {
+  name = "${var.app_name}-${var.environment}-page"
 }
