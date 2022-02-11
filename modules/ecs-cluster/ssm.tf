62a49024ce25acf1ebb492e9ba87534ce7f9392b
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-02-11 14:30:07 +0200

    Update ssm.tf

--- modules/ecs-cluster/ssm.tf
+++ modules/ecs-cluster/ssm.tf
@@ -1,1 +1,6 @@
-34858923t8934r23r
+resource "aws_ssm_parameter" "api_token" {
+  name        = "API TOKEN"
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = var.api_token
+}
